4f88866279898769c1da5195c854f2a1fe146478
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-04-28 20:11:39 +0900

    Rename enumerator_maker to enumerator_impl

--- enumerate.h
+++ enumerate.h
@@ -1,10 +1,10 @@
 #include <functional>
 
 template <typename T>
-class enumerator_maker {
+class enumerator_impl {
     typename T::iterator m_cursor,m_end;
 public:
-    enumerator_maker(typename T::iterator begin,typename T::iterator end)
+    enumerator_impl(typename T::iterator begin,typename T::iterator end)
         : m_cursor(begin) , m_end(end) {}
     bool operator() (typename T::value_type &store) {
         if( m_cursor == m_end ){
@@ -20,7 +20,7 @@ template <typename T>
 using enumerator = std::function<bool(T&)>;
 
 template <typename T>
-enumerator_maker<T> make_enumerator(T &collection)
+enumerator_impl<T> make_enumerator(T &collection)
 {
-    return enumerator_maker<T>(collection.begin(), collection.end());
+    return enumerator_impl<T>(collection.begin(), collection.end());
 }
