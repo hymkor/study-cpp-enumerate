0784422eff11f0eaa2c6754020f0b3ea76e36e2d
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-03 13:36:49 +0900

    Add example for unordered_map

--- main2.cpp
+++ main2.cpp
@@ -1,5 +1,6 @@
-#include <map>
 #include <string>
+#include <map>
+#include <unordered_map>
 
 #include "enumerate2.h"
 
@@ -7,10 +8,14 @@ extern void sub(pair_enumerator<std::string,int> &each);
 
 int main(){
     std::map<std::string,int> map;
-
     map["ahaha"] = 1;
     map["ihihi"] = 2;
     map["ufufu"] = 3;
-
     sub(make_pair_enumerator(map));
+
+    std::unordered_map<std::string,int> umap;
+    umap["AHAHA"] = 1;
+    umap["IHIHI"] = 2;
+    umap["UFUFU"] = 3;
+    sub(make_pair_enumerator(umap));
 }
