476ad6ee025049030b563d9d42064f59b506c054
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-04 15:31:46 +0900

    Simplify make_pair_enumerator

--- enumerate2.h
+++ enumerate2.h
@@ -1,11 +1,11 @@
 #include <functional>
 
 template <typename T>
-class pair_enumerator_impl {
+class make_pair_enumerator {
     typename T::iterator m_cursor,m_end;
 public:
-    pair_enumerator_impl(typename T::iterator begin,typename T::iterator end)
-        : m_cursor(begin) , m_end(end) {}
+    make_pair_enumerator(T &t)
+        : m_cursor(t.begin()) , m_end(t.end()) {}
     bool operator() (typename T::key_type    &first,
                      typename T::mapped_type &second) {
         if( m_cursor == m_end ){
@@ -20,9 +20,3 @@ public:
 
 template <typename Key,typename Mapped>
 using pair_enumerator = const std::function<bool(Key&,Mapped&)>;
-
-template <typename T>
-pair_enumerator_impl<T> make_pair_enumerator(T &collection)
-{
-    return pair_enumerator_impl<T>(collection.begin(), collection.end());
-}
