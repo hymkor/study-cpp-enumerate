a24e3a7fb731f8ab8ee76a5d91c085eb58df577b
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-04 15:33:11 +0900

    Simplify make_const_enumerator

--- cenumerate.h
+++ cenumerate.h
@@ -1,11 +1,11 @@
 #include <functional>
 
 template <typename T>
-class const_enumerator_impl {
+class make_const_enumerator {
     typename T::const_iterator m_cursor,m_end;
 public:
-    const_enumerator_impl(typename T::const_iterator begin,typename T::const_iterator end)
-        : m_cursor(begin) , m_end(end) {}
+    make_const_enumerator(const T &t)
+        : m_cursor(t.begin()) , m_end(t.end()) {}
     bool operator() (typename T::value_type &store) {
         if( m_cursor == m_end ){
             return false;
@@ -18,9 +18,3 @@ public:
 
 template <typename T>
 using enumerator = std::function<bool(T&)>;
-
-template <typename T>
-const_enumerator_impl<T> make_const_enumerator(const T &collection)
-{
-    return const_enumerator_impl<T>(collection.cbegin(), collection.cend());
-}
